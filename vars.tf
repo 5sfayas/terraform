f2b1fdcdbb4638af55e7d6f0fa5087fb7eb8e309
fayasak <travis@travis-ci.org>
2019-11-25 16:59:30 +0530

    var.tf edited with is variable to apply AMI ID based on region

--- vars.tf
+++ vars.tf
@@ -4,3 +4,11 @@ variable"AWS_REGION"{
 default="eu-west-1"
 }
 
+variable AMI{
+type="map"
+defualt= {
+    us-east-1 = "ami-13be557e"
+    us-west-2 = "ami-06b94666"
+    eu-west-1 = "ami-844e0bf7"
+	}
+}
