d2d0a3cbe47b977424bcfe057b11c616a0e8a857
fayasak <travis@travis-ci.org>
2019-11-26 18:22:27 +0530

    instance.tf file to excute shell command inside the instance (local-exec)

---
+++ output variable/instance.tf
@@ -0,0 +1,32 @@
+resource "aws_key_pair" "tom"{
+key_name="tom"
+public_key="${file("$var.PATH_TO_PUBLIC_KEY")"
+}
+
+resource "aws_instance" "example"
+{
+ami="${lookup(var.AMIS, var.AWS_REGION)"
+instance_type="t2.micro"
+key_name="${aws_key_pair.tom.key_name}"
+
+	provisioner "file"{
+	source="script.sh"
+	destination="~/fayasak/script.sh"
+	}
+
+	provisioner "remote-exec"{
+	inline=[
+	"chmod +x ~/fayasak/script.sh",
+	"sudo ~/fayasak/script.sh"
+	]
+	}
+
+	connection{
+	user="${var.INSTANCE_USERNAME}"
+	private_key="${file("${var.PATH_TO_PRIVATE_KEY}")}"
+	}
+	provisioner "local-exec"{
+		command ="echo ${aws_instance.example.private_ip}>>instanceprivateip.txt"
+
+}
+
